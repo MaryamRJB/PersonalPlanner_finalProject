4fdea588dc62ff7a659a1e1b0051bf42f06c4d3b
PersonalPlanner_finalProject <rajabimaryam870@gmail.com>
2026-08-31 02:02:16 +0330

    change email to phone

--- app/src/main/java/com/example/personalplanner/database/room/AppDatabase.java
+++ app/src/main/java/com/example/personalplanner/database/room/AppDatabase.java
@@ -32,7 +32,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
                 UserPreference.class,
                 SubTask.class
         },
-        version = 2,
+        version = 3,
         exportSchema = false
 )
 public abstract class AppDatabase extends RoomDatabase {
